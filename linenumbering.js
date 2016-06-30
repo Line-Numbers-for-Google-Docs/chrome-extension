3d53d6c274ca7d5276e2561b3858f80806a91e1d
Pablo Gamito <pablogamito@gmail.com>
2016-06-30 18:34:40 +0200

    Fully Working MVP

--- linenumbering.js
+++ linenumbering.js
@@ -1,42 +1,91 @@
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-  // First, validate the message's structure
-  if ((msg.from === 'popup') && (msg.subject === 'refresh')) {
-    // Collect the necessary data 
-    // (For your specific requirements `document.querySelectorAll(...)`
-    //  should be equivalent to jquery's `$(...)`)
-    alert("Refresh");
+//**********//
+//INITIALIZE//
+//**********//
 
-    // Directly respond to the sender (popup), 
-    // through the specified callback */
-    // response(domInfo);
+var everyXLine = 5
+
+chrome.storage.local.get(["everyXLine"], function (result) {
+  //update everyXLine value if change
+  if (result["everyXLine"] != null && result["everyXLine"] > 0 && result["everyXLine"] <= 100) {
+    everyXLine = result["everyXLine"];
   }
 });
 
-
-var everyXLine = 5;
-var lineCount = $(".kix-lineview").length;
-var index = 0;
+// var lineCount = $(".kix-lineview").length;
+var ln = 0;
 
 function numberLines() {
-  index = 0
-  $('body').css({'counter-reset':'ln'});
+  ln = 0
+  console.log("Numbering lines every " + everyXLine + " line(s).");
   $('body').find(".kix-lineview").each(function() {
-    index++;
-    if (index%everyXLine === 0) {
-      $(this).addClass("numbered");
+    ln++;
+    if (ln%everyXLine === 0) {
+      $(this).addClass("numbered").attr("ln-number", ln);
     }
   });
 }
 
-numberLines();
-
-//Update line count if change
-setInterval(function(){
-  if (lineCount != $(".kix-lineview").length) {
-    alert("running");
-    $(".numbered").removeClass("numbered");
+chrome.storage.local.get(["enabled"], function (result) {
+  if (result["enabled"] == true) {
     numberLines();
-    lineCount = $(".kix-lineview").length;
   }
-}, 1000);
+});
+
+//*****************//
+//REFRESH or UPDATE//
+//*****************//
+
+function refresh() {
+  $(".numbered").removeClass("numbered");
+  chrome.storage.local.get(["enabled"], function (result) {
+    if (result["enabled"] == true) {
+      //If extension still enabled
+      chrome.storage.local.get(["everyXLine"], function (result) {
+        //update everyXLine value if change
+        if (result["everyXLine"] == null || result["everyXLine"] < 1 || result["everyXLine"] > 100) {
+          everyXLine = 5;
+          numberLines();
+        } else {
+          everyXLine = result["everyXLine"];
+          console.log("Updated everyXLine to " + everyXLine);
+          numberLines();
+        }
+      });
+    }
+  });
+}
+
+function autorefresh(){
+  chrome.storage.local.get(["enabled"], function (result) {
+    if (result["enabled"] == true) {
+      ln = 0
+      $('body').find(".kix-lineview").each(function() {
+        ln++;
+        if (ln%everyXLine === 0) {
+          $(this).addClass("numbered").attr("ln-number", ln);
+        } else {
+          $(this).removeClass("numbered");
+        }
+      });
+    }
+  });
+}
+
+setInterval(function(){autorefresh();}, 1000);
+
+// Listen for messages from the popup
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+  // Validate the message's structure
+  if ((msg.from === 'popup') && (msg.subject === 'refresh')) {
+    //Run when popup notifies of a refresh
+    console.log("Force refresh requested");
+    refresh();
+  }
+});
+
+
+//************************//
+//SELECTION LINE NUMBERING//
+//************************//
+
+//TODO: Allow numbering lines from selection
--- popup.js
+++ popup.js
@@ -1,45 +1,51 @@
 $(document).ready(function(){
 
-  //INITIALIZE
+  //**********//
+  //INITIALIZE//
+  //**********//
+
   //Set all saved values
   chrome.storage.local.get(["enabled"], function (result) {
-    console.log(JSON.stringify(result));
+
     if (result["enabled"] == false) {
       //if no data saved for enabled keep default html unchecked
     } else {
       //if no data saved for enabled (result["enabled"] == null) by default or saved to true
       $("#enabled").attr('checked', 'checked');
     }
-  });
-  chrome.storage.local.get(["everyXLine"], function (result) {
-    console.log(JSON.stringify(result));
-    if (result["everyXLine"] == null || result["everyXLine"] < 1 || result["everyXLine"] > 100) {
-      //if no data saved saved set default to 5
-      $("#everyXLine").val("5");
-    } else {
-      //set to saved value
-      $("#everyXLine").val( result["everyXLine"] );
-    }
+
   });
 
-  //EXTENSION SETTINGS MODIFICATIONS
-  $("#enabled").change(function(){
-    //Save enabled boolean
-    chrome.storage.local.set({ "enabled": $("#enabled").is(':checked') }, function(){
-      console.log('Data saved locally.');
+  function setEveryXLineInput() {
+    chrome.storage.local.get(["everyXLine"], function (result) {
+
+      if (result["everyXLine"] == null || result["everyXLine"] < 1) {
+        //if no data saved saved set default to 5
+        $("#everyXLine").val("5");
+      } else {
+        //set to saved value
+        $("#everyXLine").val( result["everyXLine"] );
+      }
+
     });
-  });
+  }
+  setEveryXLineInput();
 
-  $("#everyXLine").change(function(){
-    //Save everyXLine value
-    if ($("#everyXLine").val() > 0 && $("#everyXLine").val() <= 100){
-      chrome.storage.local.set({ "everyXLine": $("#everyXLine").val() }, function(){
-        console.log('Data saved locally.');
-      });
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+    url: "*://docs.google.com/document/d/*"
+  }, function(tabs) {
+    if (tabs.length == 0) {
+      $(".onDocumentShow").hide();
+      $(".offDocumentShow").show();
     }
   });
 
-  //REFRESH GDOCS LINE NUMBERING WITH NEW SETTINGS
+  //**********************************************//
+  //REFRESH GDOCS LINE NUMBERING WITH NEW SETTINGS//
+  //**********************************************//
+
   function refresh(){
     chrome.tabs.query({
       url: "*://docs.google.com/document/d/*"
@@ -57,6 +63,40 @@ $(document).ready(function(){
 
   $("#refresh").click(function(){
     refresh();
+  });
+
+
+  //********************************//
+  //EXTENSION SETTINGS MODIFICATIONS//
+  //********************************//
+
+  function getCurrentPageUrl(){
+    chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, function(tabs) {
+      return tabs[0].url
+    });
+  }
+
+  $("#enabled").change(function(){
+    //Save enabled boolean
+    chrome.storage.local.set({ "enabled": $("#enabled").is(':checked') }, function(){
+      console.log('enabled value saved locally.');
+      refresh();
+    });
+  });
+
+  $("#everyXLine").change(function(){
+    //Save everyXLine value
+    if ($("#everyXLine").val() > 0){
+      chrome.storage.local.set({ "everyXLine": $("#everyXLine").val() }, function(){
+        console.log('everyXLine value saved locally.');
+        refresh();
+      });
+    } else {
+      setEveryXLineInput();
+    }
   });
 
 });
